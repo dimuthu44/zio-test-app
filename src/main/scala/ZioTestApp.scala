0505d0c6605a54d9e739304e9ecb78e9926de1ea
Dimuthu Jayamanna <dimuthu44@users.noreply.github.com>
2024-09-13 13:27:17 +0200

    Update ZioTestApp.scala

--- src/main/scala/ZioTestApp.scala
+++ src/main/scala/ZioTestApp.scala
@@ -6,7 +6,15 @@ import java.io.IOException
 object ZioTestApp extends ZIOAppDefault {
 
   def run: ZIO[Any, IOException, Unit] = myAppLogic
+  
+/**
+* 
+* The ZIO[R, E, A] data type has three type parameters: R => Either[E, A]
 
+* R - Environment Type. The effect requires an environment of type R. If this type parameter is Any, it means the effect has no requirements, because we can run the effect with any value (for example, the unit value ()).
+* E - Failure Type. The effect may fail with a value of type E. Some applications will use Throwable. If this type parameter is Nothing, it means the effect cannot fail, because there are no values of type Nothing.
+* A - Success Type. The effect may succeed with a value of type A. If this type parameter is Unit, it means the effect produces no useful information, while if it is Nothing, it means the effect runs forever (or until failure).
+*/
   val myAppLogic =
     for {
       _ <- printLine("Hello! What is your name?")
